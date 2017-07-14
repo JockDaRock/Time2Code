25491c862006668ad0f7e8cfe39aa6df8e483e3c
jocreed <jocreed@cisco.com>
2017-07-14 16:34:18 -0500

    split IDE and code excution into separate projects

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,19 @@ FROM alpine
 RUN apk add -Uuv --no-cache python3 \
     && apk upgrade -v --available --no-cache \
     && apk add ca-certificates && pip3 install --no-cache-dir --upgrade pip setuptools wheel \
-    && pip3 install requests certifi
+    && pip3 install requests certifi flask
 
 ADD https://github.com/alexellis/faas/releases/download/0.5.1-alpha/fwatchdog /usr/bin
+
 
 RUN chmod +x /usr/bin/fwatchdog
 
 WORKDIR /root/
 
-COPY time2py.py .
+COPY ide.py .
+COPY templates .
 
-ENV fprocess="python3 time2py.py"
+ENV fprocess="python3 ide.py"
 
 HEALTHCHECK --interval=1s CMD [ -e /tmp/.lock ] || exit 1
 
--- ide.py
+++ ide.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/time2code')
 @app.route('/time2code/<name>')
-def time2code(name='base'):
+def time2code(name='time2py'):
     return render_template('base_test.html', name=name)
 
 
@@ -14,7 +14,8 @@ def time2code(name='base'):
 def time2py():
     if request.method == 'POST':
         data = request.data
-        url = "http://pwd10-0-61-3-8080.host1.labs.play-with-docker.com/function/time2code"
+        host = request.host
+        url = "http://%s/function/time2py" % host
         headers = {"Content-Type": "text/plain"}
 
         code_exec = requests.post(url, data=data, headers=headers)
@@ -26,5 +27,5 @@ def time2py():
         return resp
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8181)
+    app.run(host="0.0.0.0", port=80)
 
