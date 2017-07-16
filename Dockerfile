9b0e8c9b0dbdd9d3d72b0af3092d7daa9c64271f
JockDaRock <jock.da.rock@gmail.com>
2017-07-16 02:20:37 -0500

    Changes to make flask work with FaaS

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ FROM alpine
 RUN apk add -Uuv --no-cache python3 \
     && apk upgrade -v --available --no-cache \
     && apk add ca-certificates && pip3 install --no-cache-dir --upgrade pip setuptools wheel \
-    && pip3 install requests certifi flask
+    && pip3 install requests flask
 
-# ADD https://github.com/alexellis/faas/releases/download/0.5.1-alpha/fwatchdog /usr/bin
+ADD https://github.com/alexellis/faas/releases/download/0.5.1-alpha/fwatchdog /usr/bin
 
-# RUN chmod +x /usr/bin/fwatchdog
+RUN chmod +x /usr/bin/fwatchdog
 
 WORKDIR /root/
 
@@ -18,6 +18,4 @@ ENV fprocess="python3 ide.py"
 
 HEALTHCHECK --interval=1s CMD [ -e /tmp/.lock ] || exit 1
 
-EXPOSE 5000
-
-CMD ["python3", "./ide.py"]
+CMD ["fwatchdog"]
--- ide.py
+++ ide.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import requests
+from wsgiref.handlers import CGIHandler
 
 app = Flask(__name__)
 
@@ -22,10 +23,25 @@ def time2py():
 
         resp = code_exec.text
 
-        print(resp)
+        # print(resp)
 
         return resp
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
 
+class ProxyFix(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['SERVER_NAME'] = "localhost"
+        environ['SERVER_PORT'] = "8080"
+        environ['REQUEST_METHOD'] = "GET"
+        environ['SCRIPT_NAME'] = ""
+        environ['PATH_INFO'] = "/"
+        environ['QUERY_STRING'] = ""
+        environ['SERVER_PROTOCOL'] = "HTTP/1.1"
+        return self.app(environ, start_response)
+
+if __name__ == '__main__':
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    CGIHandler().run(app)
