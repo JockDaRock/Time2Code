3a0c89e86d398eb59c369466646ebc8a4071fd1c
JockDaRock <jock.da.rock@gmail.com>
2017-08-03 00:02:54 -0500

    minikube / k8s automation updates

--- minikube.sh
+++ minikube.sh
@@ -8,6 +8,13 @@ curl -sSL cli.get-faas.com | sudo sh
 
 kubectl apply -f faas-netes/faas.yml,faas-netes/monitoring.yml,./time2code-server-k8s.yml
 
+kubectl get pods > .pod_running
+
+while [ $(awk '/gateway/ {print $3}' .pod_running) != "Running" ]; do
+    echo "waiting for faas to start"
+    sleep 1
+    kubectl get pods > .pod_running
+
 sed "s/localhost/$(minikube ip)/" time2code-faas-cli-functions.yml > .time2code-faas-cli-minikube.yml
 
 faas-cli -action deploy -f .time2code-faas-cli-minikube.yml
