e492cc1c16587e161bb1313f51d138c613e1bc7a
Jock Reed <jock.da.rock@gmail.com>
2017-10-12 08:46:12 -0500

    update for code copying to IDE, fixed bug with the lang query parameter
    Signed-off-by: JockDaRock <jock.da.rock@gmail.com>

--- ide-server.py
+++ ide-server.py
@@ -42,7 +42,7 @@ def time2code():
     if lang:
         code_lang = lang
     else:
-        code_lang = "python"
+        code_lang = "python3"
 
     return render_template('index-panel.html', code_text=code_text, code_lang=code_lang)
 
@@ -71,6 +71,7 @@ def code():
 @app.route('/tutorial')
 def tutorial():
     text = request.args.get('code')
+    lang = request.args.get('lang')
     straight_text = request.args.get('straight_text')
     get_tut = request.args.get('tut')
     code_text = ""
@@ -92,8 +93,13 @@ def tutorial():
     elif straight_text:
         code_text = straight_text
 
+    if lang:
+        code_lang = lang
+    else:
+        code_lang = "python3"
+
     content = Markup(markdown.markdown(mark, extensions=['pymdownx.github', 'pymdownx.highlight']))
-    return render_template('index-tut.html', markdown=content, code_text=code_text)
+    return render_template('index-tut.html', markdown=content, code_text=code_text, code_lang=code_lang)
 
 
 if __name__ == '__main__':
