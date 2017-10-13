c0386da254779afeabce81e69ad2483a509b7534
JockDaRock <jock.da.rock@gmail.com>
2017-10-12 23:54:31 -0500

    update Swarm deploy script
    Signed-off-by: JockDaRock <jock.da.rock@gmail.com>

--- time2deploy.sh
+++ time2deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Deploying Time2Code on FaaS"
+curl -O https://raw.githubusercontent.com/JockDaRock/Time2Code/master/time2code-swarm-deploy.yml
 docker stack deploy time2code --compose-file time2code-swarm-deploy.yml
